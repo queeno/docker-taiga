db5a7070796cf24d1272c1fa237564635ee2def8
Simon Aquino <simon.aquino@kpmg.co.uk>
2015-10-13 11:24:45 +0100

    Remove redundant lines in build.sh
    The local.py file will be copied separately from the repository. This
    commit removes redundant lines fixing the local.py file in the
    repository.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+set -e
+
 if [[ $OSTYPE != darwin* ]]; then
   SUDO=sudo
 fi
@@ -18,16 +20,16 @@ git clone -b stable --single-branch https://github.com/taigaio/taiga-front-dist
 if [[ $OSTYPE != darwin* ]]; then
     sed -i 's/^enum34/#enum34/' taiga-back/requirements.txt
     sed -i -e '/sample_data/s/^/#/' taiga-back/regenerate.sh
-    sed -i -e 's/django.db.backends.postgresql_psycopg2/transaction_hooks.backends.postgresql_psycopg2/g' settings/local.py
 else
     sed -i '.bak' 's/^enum34/#enum34/' taiga-back/requirements.txt
     sed -i '.bak' '/sample_data/s/^/#/' taiga-back/regenerate.sh
-    sed -i 's/django.db.backends.postgresql_psycopg2/transaction_hooks.backends.postgresql_psycopg2/g' settings/local.py
 fi
 
 cp taiga-back/requirements.txt .
 
 $SUDO docker build -t ipedrazas/taiga .
+
+rm requirements.txt
 
 if [[ -d  taiga-back ]]; then
   rm -rf taiga-back

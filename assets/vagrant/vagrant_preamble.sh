f6553c7be377f54aa5f0be8e9cffbd823975d8a6
Simon Aquino <simon.aquino@kpmg.co.uk>
2015-10-13 18:12:57 +0100

    Build and run docker taiga container in vagrant
    When provisioning a new vagrant box to text taiga-docker, the preamble
    script will now automatically build a taiga docker image for you, run it
    along postgres, initialise and populate the DB, and create volume
    symlinks.

--- assets/vagrant/vagrant_preamble.sh
+++ assets/vagrant/vagrant_preamble.sh
@@ -12,6 +12,38 @@ echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sour
 # Update local apt cache
 apt-get -qq update
 
-# Install docker
+# Install docker (and JSON Parser)
 apt-get install -y \
+  jq \
   docker-engine=${DOCKER_VERSION}
+
+# Build the taiga docker container
+cd /vagrant
+./build.sh
+
+# Run the postgres container
+docker run -d --name postgres postgres
+
+# Sleep for 5 seconds to allow postgres to start
+sleep 5
+
+# Initialise the database
+docker exec postgres sh -c "su postgres --command 'createuser -d -r -s taiga'"
+docker exec postgres sh -c "su postgres --command 'createdb -O taiga taiga'"
+
+# Run taiga
+docker run -d -p 8000:8000 --name taiga --link postgres:postgres ipedrazas/taiga
+
+# Populate the database
+docker exec taiga bash regenerate.sh
+
+# Symlink the docker volumes in /root
+DOCKER_INSPECT="$(docker inspect taiga)"
+
+MOUNT0=($(echo ${DOCKER_INSPECT} | tee >(jq -r ".[0].Mounts[0].Source") >(jq -r ".[0].Mounts[0].Destination") 1> /dev/null))
+MOUNT1=($(echo ${DOCKER_INSPECT} | tee >(jq -r ".[0].Mounts[1].Source") >(jq -r ".[0].Mounts[1].Destination") 1> /dev/null))
+MOUNT2=($(echo ${DOCKER_INSPECT} | tee >(jq -r ".[0].Mounts[2].Source") >(jq -r ".[0].Mounts[2].Destination") 1> /dev/null))
+
+ln -s "${MOUNT0[1]}" "/root/$(basename ${MOUNT0[0]})"
+ln -s "${MOUNT1[1]}" "/root/$(basename ${MOUNT1[0]})"
+ln -s "${MOUNT2[1]}" "/root/$(basename ${MOUNT2[0]})"

4a68bbd34b0889f128b129ad3d48720297fabdd8
Simon Aquino <simon.aquino@kpmg.co.uk>
2015-11-12 15:11:35 +0000

    Commenting out gmail settings in docker-settings.py
    Users can turn them on demand if needed.

--- assets/config/docker-settings.py
+++ assets/config/docker-settings.py
@@ -123,12 +123,12 @@ STATIC_URL = "http://" + os.getenv("API_NAME") + "/static/"
 #DEFAULT_FROM_EMAIL = "john@doe.com"
 
 # GMAIL SETTINGS EXAMPLE
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'taiga@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'taiga@gmail.com'
+#EMAIL_HOST_PASSWORD = 'password'
 
 # THROTTLING
 #REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
